f9b75a3c399b60db90500c88f8ccd96cabc6ddc2
dextermallo <dextermallo@gmail.com>
2023-07-18 16:50:29 +0100

    CHORE: cleanup; remove rules (use --before for further test)

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-rm -rf data tmp report
-rm -rf tests/docker-compose-after.yml
-rm -rf tests/docker-compose-after.yml-e
-rm -rf tests/docker-compose-before.yml
-rm -rf tests/docker-compose-before.yml-e
-rm -rf tests/logs
-
-docker kill $(docker ps -aq) && docker rm $(docker ps -aq)
--- test.py
+++
@@ -1,5 +0,0 @@
-from ping3 import ping
-
-
-res = ping("http://localhost:80", timeout=5)
-print(res)
