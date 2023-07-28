b775fb238e8a7f4e1f14dbf8547f75d026136662
dextermallo <dextermallo@gmail.com>
2023-07-28 20:29:10 +0100

    FEAT: colorized logs

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,29 @@
 import logging
 
 
-# set up logger
-formatter = logging.Formatter('[%(levelname)s] %(asctime)s: %(message)s')
+class Formatter(logging.Formatter):
+    __grey = "\x1b[38;20m"
+    __yellow = "\x1b[33;20m"
+    __red = "\x1b[31;20m"
+    __bold_red = "\x1b[31;1m"
+    __reset = "\x1b[0m"
+    __format = "[%(levelname)s] %(message)s"
+
+    __FORMATS = {
+        logging.DEBUG: __grey + __format + __reset,
+        logging.INFO: __format,
+        logging.WARNING: __yellow + __format + __reset,
+        logging.ERROR: __red + __format + __reset,
+        logging.CRITICAL: __bold_red + __format + __reset
+    }
+
+    def format(self, record):
+        log_fmt = self.__FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-handler.setFormatter(formatter)
+handler.setFormatter(Formatter())
+logger.setLevel(logging.INFO)
 logger.addHandler(handler)
-logger.setLevel(logging.WARNING)
